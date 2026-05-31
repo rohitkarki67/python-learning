5c13c506ca92db4d2aa160c448f6003ced41c288
rohitkarki67 <rohit.karki4068@gmail.com>
2026-05-31 21:16:32 +0530

    First project: Greeting Machine

---
+++ GreetingMachine.py
@@ -0,0 +1,11 @@
+"""GreetingMachine.py
+
+What this program should do:
+- ask the user for their name
+- print a greeting message
+"""
+
+print("Welcome")
+user_name = input("what is your name? ")
+print(f"Hello {user_name}!")
+print("welcome to python programming")
