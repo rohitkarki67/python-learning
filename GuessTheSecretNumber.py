b45f55091533c50e4660366f043916eb59888a63
rohitkarki67 <rohit.karki4068@gmail.com>
2026-06-08 13:40:11 +0530

    Add Secret Number Checker

---
+++ GuessTheSecretNumber.py
@@ -0,0 +1,21 @@
+"""
+What this program should do:
+- Set a secret number
+- Ask the user to guess the secret number
+- If the user guesses the secret number, print a congratulatory message
+- If the user does not guess the secret number, print a message they guessed wrong.
+
+"""
+
+
+secret_number = 7 # Hehehe Ronaldo jersey number
+
+print("Hey there! Can you guess the secret number?")
+
+user_inpit = float(input("Enter your guess: "))
+
+if user_inpit == secret_number:
+    print("Great you gussed right.")
+
+else:
+    print("You gussed it wrong.")
